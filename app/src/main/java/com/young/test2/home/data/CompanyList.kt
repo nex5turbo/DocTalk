12ee157aac421cd06d7e2a4feb6c79374b960c25
Won Young <nex5turbo@gmail.com>
2021-09-10 19:28:22 +0900

    create data for home.json response <ResponseHome.kt>

---
+++ app/src/main/java/com/young/test2/home/data/CompanyList.kt
@@ -0,0 +1,11 @@
+package com.young.test2.home.data
+
+import com.google.gson.annotations.SerializedName
+import com.young.test2.home.HomeRecyclerItem
+
+data class CompanyList(
+
+    @SerializedName("companyList")
+    var companyList: List<CompanyItem>? = null
+
+): HomeRecyclerItem
---
+++ app/src/main/java/com/young/test2/home/data/ResponseHome.kt
@@ -0,0 +1,37 @@
+package com.young.test2.home.data
+
+import com.google.gson.annotations.SerializedName
+
+data class ResponseHome(
+    @SerializedName("pageMap")
+    var pageMap: PageMap? = null,
+
+    @SerializedName("expertListPosition")
+    var expertListPosition: Int? = null,
+
+    @SerializedName("companyListPosition")
+    var companyListPosition: Int? = null,
+
+    @SerializedName("consultList")
+    var consultList: List<ConsultItem>? = null,
+
+    @SerializedName("expertList")
+    var expertList: ExpertList? = null,
+
+    @SerializedName("companyList")
+    var companyList: CompanyList? = null
+){
+    data class PageMap(
+        @SerializedName("pageNumber")
+        var pageNumber: Int? = null,
+
+        @SerializedName("rowPerPage")
+        var rowPerPage: Int? = null,
+
+        @SerializedName("pageCount")
+        var pageCount: Int? = null,
+
+        @SerializedName("totalCount")
+        var totalCount: Int? = null
+    )
+}
