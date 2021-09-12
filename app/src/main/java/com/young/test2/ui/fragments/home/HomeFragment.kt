41513a5cec6cba87500690c31d28285fab97e103
Won Young <nex5turbo@gmail.com>
2021-09-13 01:14:51 +0900

    catch no result, network error exception

--- app/src/main/java/com/young/test2/ui/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/young/test2/ui/fragments/home/HomeFragment.kt
@@ -6,23 +6,36 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModelProvider
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.young.test2.databinding.FragmentHomeBinding
 
 class HomeFragment: Fragment() {
     private lateinit var _binding: FragmentHomeBinding
     val binding: FragmentHomeBinding get() = _binding
+    val viewModel: HomeFragmentViewModel by
+        lazy{ ViewModelProvider(requireActivity()).get(HomeFragmentViewModel::class.java) }
+
+    val refeshListener = SwipeRefreshLayout.OnRefreshListener {
+        viewModel.getList()
+    }
     val TAG = "HomeFragment"
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val viewModel = ViewModelProvider(requireActivity()).get(HomeFragmentViewModel::class.java)
-
         _binding.viewModel = viewModel
         _binding.lifecycleOwner = viewLifecycleOwner
+
+        _binding.homeRefreshLayout.setOnRefreshListener(refeshListener)
+
+        viewModel.homeItemList.observe(viewLifecycleOwner, {
+            _binding.homeRefreshLayout.isRefreshing = false
+        })
 
         return _binding.root
     }
--- app/src/main/java/com/young/test2/ui/fragments/home/HomeFragmentViewModel.kt
+++ app/src/main/java/com/young/test2/ui/fragments/home/HomeFragmentViewModel.kt
@@ -21,19 +21,34 @@ class HomeFragmentViewModel: ViewModel(){
     private var _pbVisibility: MutableLiveData<Int> = MutableLiveData(View.INVISIBLE)
     val pbVisibility: LiveData<Int> get() = _pbVisibility
 
+    private var _noResultVisibility: MutableLiveData<Int> = MutableLiveData(View.INVISIBLE)
+    val noResultVisibility: LiveData<Int> get() = _noResultVisibility
+
+    private var _errorVisibility: MutableLiveData<Int> = MutableLiveData(View.INVISIBLE)
+    val errorVisibility: LiveData<Int> get() = _errorVisibility
+
     init {
         getList()
     }
 
     fun getList(){
+        _homeItemList.value!!.clear()
         _pbVisibility.value = View.VISIBLE
+        _noResultVisibility.value = View.INVISIBLE
+        _errorVisibility.value = View.INVISIBLE
+
         repository.getHomeList(
             onResponse = {
                 _homeItemList.value = it
+                if (it.isEmpty()) {
+                    _noResultVisibility.value = View.VISIBLE
+                }
                 _pbVisibility.value = View.INVISIBLE
             },
             onFailure = {
                 Log.e("HomeFragment", it.toString())
+                _homeItemList.value = arrayListOf()
+                _errorVisibility.value = View.VISIBLE
                 _pbVisibility.value = View.INVISIBLE
             }
         )
