debf9b765c886905a80ff8c5ef28a3f9a3e61052
Won Young <nex5turbo@gmail.com>
2021-09-12 23:26:02 +0900

    repository model modified

--- app/src/main/java/com/young/test2/repository/RemoteDataSource.kt
+++ app/src/main/java/com/young/test2/repository/RemoteDataSource.kt
@@ -1,9 +1,10 @@
 package com.young.test2.repository
 
-import com.young.test2.home.data.ResponseHome
+import com.young.test2.data.HomeRecyclerItem
+import com.young.test2.data.ResponseHome
 import retrofit2.Response
 
 interface RemoteDataSource {
-    fun getHomeList(onResponse: (Response<ResponseHome>) -> Unit,
+    fun getHomeList(onResponse: (ArrayList<HomeRecyclerItem>) -> Unit,
                     onFailure: (Throwable) -> Unit)
 }
--- app/src/main/java/com/young/test2/repository/RemoteDataSourceImpl.kt
+++ app/src/main/java/com/young/test2/repository/RemoteDataSourceImpl.kt
@@ -1,19 +1,48 @@
 package com.young.test2.repository
 
-import com.young.test2.home.data.ResponseHome
-import com.young.test2.home.retrofit.HomeApi
+import com.young.test2.data.CompanyList
+import com.young.test2.data.ExpertList
+import com.young.test2.data.HomeRecyclerItem
+import com.young.test2.data.ResponseHome
+import com.young.test2.retrofit.DocApi
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class RemoteDataSourceImpl: RemoteDataSource {
     override fun getHomeList(
-        onResponse: (Response<ResponseHome>) -> Unit,
+        onResponse: (ArrayList<HomeRecyclerItem>) -> Unit,
         onFailure: (Throwable) -> Unit
     ) {
-        HomeApi.getHomeService().getHomeList().enqueue(object : Callback<ResponseHome> {
+        DocApi.getDocService().getHomeList().enqueue(object : Callback<ResponseHome> {
             override fun onResponse(call: Call<ResponseHome>, response: Response<ResponseHome>) {
-                onResponse(response)
+                val resultList = arrayListOf<HomeRecyclerItem>()
+                if (response.isSuccessful) {
+                    val body = response.body()!!
+
+                    val expertPosition = body.expertListPosition
+                    val companyPosition = body.companyListPosition
+
+                    val consultList = body.consultList
+                    val expertList = ExpertList(body.expertList)
+                    val companyList = CompanyList(body.companyList)
+
+
+                    var index = 1
+                    for (consult in consultList!!) {
+                        if (index == expertPosition) {
+                            resultList.add(expertList)
+                            index++
+                        } else if (index == companyPosition) {
+                            resultList.add(companyList)
+                            index++
+                        }
+                        consult.dateToString()
+                        resultList.add(consult)
+                        index++
+                    }
+                }
+                onResponse(resultList)
             }
 
             override fun onFailure(call: Call<ResponseHome>, t: Throwable) {
